5657a2ad10f8c07d74ea9cf731bd643a8c3835f9
Xmde <erve2005@gmail.com>
2021-11-06 13:24:01 -0700

    updated ecosystem.config for beta

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,15 @@
 module.exports = {
-  apps : [{
-    name   : "bxt",
-    script : "./index.js",
-    instance_var: 'INSTANCE_ID',
-    env: {
-       NODE_ENV: "production"
+  apps: [
+    {
+      name: 'bxt_beta',
+      script: './index.js',
+      instance_var: 'INSTANCE_ID',
+      env: {
+        NODE_ENV: 'production',
+      },
+      watch: true,
+      restart_delay: 5000,
+      log_date_format: 'YYYY-MM-DDTHH:mm:ss',
     },
-    watch : true,
-    restart_delay: 5000,
-    log_date_format : "YYYY-MM-DDTHH:mm:ss"
-  }]
-}
+  ],
+};
